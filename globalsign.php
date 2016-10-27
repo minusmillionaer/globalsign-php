6df3a8ce7ca70604152285b7400fe0b5668b91c6
Giuliano <gschindler@giulianoschindler.com>
2016-10-27 21:57:20 +0000

    Applied fixes from StyleCI

--- globalsign.php
+++ globalsign.php
@@ -4,23 +4,23 @@ $url = 'https://test-gcc.globalsign.com/qb/ws/GasQuery?wsdl';
 $nameSpace = 'http://stub.order.gasapiserver.esp.globalsign.com';
 $client = new SoapClient($url);
 $client->_namespace = $nameSpace;
-$params = array( 'OVOrder' => array(
+$params = ['OVOrder' => [
 
-		'Request' => array(
-			'OrderRequestHeader' => array(
-				'AuthToken' => array(
-					'UserName' => 'PAR0_00000',
-					'Password' => 'xxxx'
-				)
-			),
-		'OrderRequestParameter' => array(
-		'ProductCode' => 'OV',
-		'OrderKind' => 'new',
-		'Licenses' => '1',
-		'ValidityPeriod' => array(
-			'Months' => '12',
-		),
-		'CSR' => '-----BEGIN CERTIFICATE REQUEST-----
+        'Request' => [
+            'OrderRequestHeader' => [
+                'AuthToken' => [
+                    'UserName' => 'PAR0_00000',
+                    'Password' => 'xxxx',
+                ],
+            ],
+        'OrderRequestParameter' => [
+        'ProductCode'    => 'OV',
+        'OrderKind'      => 'new',
+        'Licenses'       => '1',
+        'ValidityPeriod' => [
+            'Months' => '12',
+        ],
+        'CSR' => '-----BEGIN CERTIFICATE REQUEST-----
 MIIBvzCCASgCAQAwbDELMAkGA1UEBhMCQkUxEDAOBgNVBAgTB0JyYWJhbnQxDzAN
 BgNVBAcTBkxldXZlbjETMBEGA1UEChMKR2xvYmFsU2lnbjEMMAoGA1UECxMDd2Vi
 MRcwFQYDVQQDEw53d3cucGpiMTU0LmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAw
@@ -32,37 +32,37 @@ CIiCF55Z/hB0lxyM11KzdO5cMBw+nmJN+6lIFyfyPGhaOW9I/ACVCDSwY/QYRMh7
 LqUex4+xr2t2C8oTIqp3iJMl8Hs+X9hVlnB4WjdiNbnkfgYH89UyQUPVbKxQNJ2F
 kT+h2sEZYU7k2ZwYoVnS212G1w==
 -----END CERTIFICATE REQUEST-----',
-		'Options' => array(
-			'Option' => array(
-				'OptionName' => 'SAN',
-				'OptionValue' => 'false',
-			),
-		),
-		),
-		'SubID'=> NULL,
-		'ContactInfo' => array(
-			'FirstName' => 'John',
-			'LastName' => 'Doe',
-			'Phone' => '00000000000',
-			'Email' => 'admin@johndoe.com',
-		),
-		'OrganizationInfo' => array(
-			'OrganizationName' => 'John Doe Inc.',
-				'OrganizationAddress' => array(
-					'AddressLine1' => 'asdfasdf 12',
-					'City' => 'Cologne',
-					'Region' => 'NRW',
-					'PostalCode' => '51149',
-					'Country' => 'DE',
-					'Phone' => '00000000000',
-				),
-			),
+        'Options' => [
+            'Option' => [
+                'OptionName'  => 'SAN',
+                'OptionValue' => 'false',
+            ],
+        ],
+        ],
+        'SubID'       => null,
+        'ContactInfo' => [
+            'FirstName' => 'John',
+            'LastName'  => 'Doe',
+            'Phone'     => '00000000000',
+            'Email'     => 'admin@johndoe.com',
+        ],
+        'OrganizationInfo' => [
+            'OrganizationName'        => 'John Doe Inc.',
+                'OrganizationAddress' => [
+                    'AddressLine1' => 'asdfasdf 12',
+                    'City'         => 'Cologne',
+                    'Region'       => 'NRW',
+                    'PostalCode'   => '51149',
+                    'Country'      => 'DE',
+                    'Phone'        => '00000000000',
+                ],
+            ],
 
-		),
-	),
+        ],
+    ],
 
-);
+];
 //var_dump($params);
 $res = $client->__soapCall('OVOrder', $params);
-echo "<pre>";
+echo '<pre>';
 var_dump($res);
